a9f1183ddb0e5753cd1135b20802c5f1e26b04b4
MilosRadin <milosradin86@gmail.com>
2020-10-12 20:55:47 +0200

    Initial commit

---
+++ app/src/main/java/com/ftninfomatika/cas_016/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ftninfomatika.cas_016;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ogranicavajuci_raspored);
+    }
+}
